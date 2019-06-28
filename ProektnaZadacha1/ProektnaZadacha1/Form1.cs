86bca0900951e0103aea09bdf38c52c065666b75
ma7ko <49078296+ma7ko@users.noreply.github.com>
2019-06-28 14:27:20 +0200

    Проектна задача по „Визуелно програмирање“

---
+++ ProektnaZadacha1/ProektnaZadacha1/Form1.cs
@@ -0,0 +1,172 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using ProektnaZadacha1.Properties;
+
+namespace ProektnaZadacha1
+{
+    public partial class Form1 : Form
+    {
+        Pacman pacman;
+        public static readonly int widthw = 17;
+        public static readonly int heightw = 10;
+        Timer timer;
+        Image goodFood;
+        Image badFood;
+        bool[][] goodFoodMap;
+        bool[][] badFoodMap;
+        public Form1()
+        {
+            InitializeComponent();
+            pacman = new Pacman();
+            goodFood = Resources.apple1;
+            badFood = Resources.candy;
+            DoubleBuffered = true;
+            initializeMatrix();
+        }
+
+        private void initializeMatrix()
+        {
+            goodFoodMap = new bool[heightw][];
+            badFoodMap = new bool[heightw][];
+            for(int i = 0; i < heightw; i++)
+            {
+                goodFoodMap[i] = new bool[widthw];
+                badFoodMap[i] = new bool[widthw];
+                for(int j = 0; j < widthw; j++)
+                {
+                    goodFoodMap[i][j] = false;
+                    badFoodMap[i][j] = false;
+                }
+            }
+
+            int randombroj = 0;
+            for(int i = 0; i < heightw; i++)
+            {
+                Random random = new Random();
+                int brojr = random.Next(i, heightw);
+                for (int j = 0; j < widthw; j++)
+                {
+                    int brojk = random.Next(j, widthw);
+                    if (randombroj % 3 == 0)
+                    {
+                        if (goodFoodMap[brojr][brojk] == false && badFoodMap[brojr][brojk] == false)
+                        {
+                            goodFoodMap[brojr][brojk] = true;
+                        }
+                        randombroj++;
+                    }
+                    else
+                    {
+                        if (badFoodMap[brojr][brojk] == false && goodFoodMap[brojr][brojk] == false)
+                        {
+                            badFoodMap[brojr][brojk] = true;
+                        }
+                        randombroj++;
+                    }
+                }
+            }
+            timer = new Timer();
+            timer.Interval = 250;
+            timer.Tick += new EventHandler(timer1_Tick);
+            timer.Start();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Paint(object sender, PaintEventArgs e)
+        {
+            Graphics g = e.Graphics;
+            g.Clear(Color.White);
+            for (int i = 0; i < goodFoodMap.Length; i++)
+            {
+                for (int j = 0; j < goodFoodMap.Length; j++)
+                {
+                    if (goodFoodMap[i][j])
+                    {
+                        g.DrawImageUnscaled(goodFood, j * Pacman.initialRadius * 2 + (Pacman.initialRadius * 2 - goodFood.Height) / 2, i * Pacman.initialRadius * 2 + (Pacman.initialRadius * 2 - goodFood.Width) / 2);
+                    }
+                    else if (badFoodMap[i][j])
+                    {
+                        g.DrawImageUnscaled(badFood, j * Pacman.initialRadius * 2 + (Pacman.initialRadius * 2 - badFood.Height) / 2, i * Pacman.initialRadius * 2 + (Pacman.initialRadius * 2 - badFood.Width) / 2);
+                    }
+                }
+            }
+            pacman.Draw(g);
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            int calculatedXPos = Math.Abs(pacman.Xpos / 40);
+            int calculatedYpos = Math.Abs(pacman.Ypos / 38);
+
+            //so it wont go out of bounds
+            if (calculatedXPos == widthw)
+            {
+                calculatedXPos--;
+            }
+            if (calculatedYpos == heightw)
+            {
+                calculatedYpos--;
+            }
+
+            //check if there is food on the coords and eat it
+            if (goodFoodMap[calculatedYpos][calculatedXPos])
+            {
+                goodFoodMap[calculatedYpos][calculatedXPos] = false;
+                pacman.DecreaseRadius();
+            }
+            if (badFoodMap[calculatedYpos][calculatedXPos])
+            {
+                badFoodMap[calculatedYpos][calculatedXPos] = false;
+                pacman.IncreaseRadius();
+            }
+            pacman.Move();
+            Invalidate(true);
+        }
+
+        private void Form1_KeyUp(object sender, KeyEventArgs e)
+        {
+            switch (e.KeyCode)
+            {
+                case Keys.Up:
+                    if (pacman.Direction != Pacman.DIRECTION.up)
+                    {
+                        pacman.ChangeDirection(Pacman.DIRECTION.up);
+                    }
+                    break;
+                case Keys.Down:
+                    if (pacman.Direction != Pacman.DIRECTION.down)
+                    {
+                        pacman.ChangeDirection(Pacman.DIRECTION.down);
+                    }
+                    break;
+                case Keys.Left:
+                    if (pacman.Direction != Pacman.DIRECTION.left)
+                    {
+                        pacman.ChangeDirection(Pacman.DIRECTION.left);
+                    }
+                    break;
+                case Keys.Right:
+                    if (pacman.Direction != Pacman.DIRECTION.right)
+                    {
+                        pacman.ChangeDirection(Pacman.DIRECTION.right);
+                    }
+                    break;
+                default:
+                    break;
+            }
+
+            Invalidate();
+        }
+    }
+}
---
+++ ProektnaZadacha1/ProektnaZadacha1/Pacman.cs
@@ -0,0 +1,149 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Drawing;
+
+namespace ProektnaZadacha1
+{
+    class Pacman
+    {
+        public int Xpos { get; set; }
+        public int Ypos { get; set; }
+        public enum DIRECTION
+        {
+            right,
+            left,
+            up,
+            down
+        };
+        public DIRECTION Direction;
+        public int Radius = 20;
+        public static int initialRadius = 20;
+        public int Speed { get; set; }
+        public bool IsOpen { get; set; }
+        public bool blocked { get; set; }
+        public SolidBrush brush = new SolidBrush(Color.Yellow);
+
+        public Pacman()
+        {
+            Speed = Radius;
+            Xpos = 7;
+            Ypos = 5;
+            Direction = DIRECTION.right;
+            IsOpen = true;
+            blocked = false;
+        }
+
+        public void ChangeDirection(DIRECTION direction)
+        {
+            // vasiot kod ovde
+            Direction = direction;
+        }
+
+        //Check if the mouth is open and move in the correct way with its speed
+        public void Move()
+        {
+            if (IsOpen)
+            {
+                IsOpen = false;
+            }
+            else
+            {
+                IsOpen = true;
+            }
+            switch (Direction)
+            {
+                case DIRECTION.right:
+                    if (Xpos >= 595)
+                    {
+                        Xpos = -25;
+                    }
+                    else
+                    {
+                        Xpos += Speed;
+                    }
+
+                    break;
+                case DIRECTION.left:
+                    if (Xpos <= 0)
+                    {
+                        Xpos = 600;
+                    }
+                    else
+                    {
+                        Xpos -= Speed;
+                    }
+                    break;
+                case DIRECTION.up:
+                    if (Ypos <= 0)
+                    {
+                        Ypos = 390;
+                    }
+                    else
+                    {
+                        Ypos -= Speed;
+                    }
+                    break;
+                case DIRECTION.down:
+                    if (Ypos >= 380)
+                    {
+                        Ypos = -25;
+                    }
+                    else
+                    {
+                        Ypos += Speed;
+                    }
+                    break;
+            }
+        }
+
+        public void IncreaseRadius()
+        {
+            Radius += (Radius * 20) / 100;
+        }
+
+        public void DecreaseRadius()
+        {
+            Radius -= (Radius * 20) / 100;
+        }
+
+        //Check pacmans direction and if the mouth should be open or closed and draw him
+        public void Draw(Graphics g)
+        {
+            if (Direction == DIRECTION.right && IsOpen)
+            {
+                g.FillPie(brush, Xpos, Ypos, Radius + Radius, Radius + Radius, 40, 280);
+            }
+            else if (Direction == DIRECTION.right && !IsOpen)
+            {
+                g.FillEllipse(brush, Xpos, Ypos, Radius + Radius, Radius + Radius);
+            }
+            else if (Direction == DIRECTION.left && IsOpen)
+            {
+                g.FillPie(brush, Xpos, Ypos, Radius + Radius, Radius + Radius, 220, 280);
+            }
+            else if (Direction == DIRECTION.left && !IsOpen)
+            {
+                g.FillEllipse(brush, Xpos, Ypos, Radius + Radius, Radius + Radius);
+            }
+            else if (Direction == DIRECTION.up && IsOpen)
+            {
+                g.FillPie(brush, Xpos, Ypos, Radius + Radius, Radius + Radius, 310, 280);
+            }
+            else if (Direction == DIRECTION.up && !IsOpen)
+            {
+                g.FillEllipse(brush, Xpos, Ypos, Radius + Radius, Radius + Radius);
+            }
+            else if (Direction == DIRECTION.down && IsOpen)
+            {
+                g.FillPie(brush, Xpos, Ypos, Radius + Radius, Radius + Radius, 130, 280);
+            }
+            else if (Direction == DIRECTION.down && !IsOpen)
+            {
+                g.FillEllipse(brush, Xpos, Ypos, Radius + Radius, Radius + Radius);
+            }
+        }
+    }
+}
